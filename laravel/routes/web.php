5baf89ba21163a1927412ff1d4d5a72477bbc4c3
hudamustafa <hudamustafa00@gmail.com>
2022-11-08 21:05:57 +0800

    Update web.php

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -29,9 +29,9 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'App\Http\Controllers\ProfileController@edit']);
 	Route::put('profile', ['as' => 'profile.update', 'uses' => 'App\Http\Controllers\ProfileController@update']);
 	Route::get('upgrade', function () {return view('pages.upgrade');})->name('upgrade');
-	 Route::get('map', function () {return view('pages.maps');})->name('map');
-	 Route::get('icons', function () {return view('pages.icons');})->name('icons');
-	 Route::get('table-list', function () {return view('pages.tables');})->name('table');
+	Route::get('map', function () {return view('pages.maps');})->name('map');
+	Route::get('icons', function () {return view('pages.icons');})->name('icons');
+	Route::get('table-list', function () {return view('pages.tables');})->name('table');
 	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
     Route::get('/marital', 'App\Http\Controllers\MaritalController@index')->name('marital.index');
     Route::get('/staff', 'App\Http\Controllers\StaffController@index')->name('staff.index');
@@ -43,11 +43,12 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::post('/delete/{id}', 'App\Http\Controllers\NgoController@get_delete')->name('ngo.delete');
 	Route::get('/announce', 'App\Http\Controllers\AnnouncementController@index');
 	Route::get('/application', 'App\Http\Controllers\ApplicationController@index')->name('application.index');
+	
 	//activity
     Route::get('/activity-list', 'App\Http\Controllers\ActivityController@index')->name('activity.index');
     Route::get('/create-activity', 'App\Http\Controllers\ActivityController@create')->name('activity.create');
-    Route::post('/store-activity', 'App\Http\Controllers\ActivityController@store')->name('activity.store');
     Route::get('/edit-activity/{id}', 'App\Http\Controllers\ActivityController@edit')->name('activity.edit');
+    Route::post('/store-activity', 'App\Http\Controllers\ActivityController@store')->name('activity.store');
     Route::post('/update-activity/{id}', 'App\Http\Controllers\ActivityController@update')->name('activity.update');
-    Route::post('/delete-activity', 'App\Http\Controllers\ActivityController@delete')->name('activity.delete');
+    Route::post('/delete-activity/{id}', 'App\Http\Controllers\ActivityController@delete')->name('activity.delete');
 });
